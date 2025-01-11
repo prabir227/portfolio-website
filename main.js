d83ae7c9cc53607c1842dd87d3355776fbfc1508
Prabir Ghosh <prabirghosh227@gmail.com>
2025-01-11 18:52:57 +0530

    added emailjs fo contact form

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ var typed = new Typed('#desig', {
       backSpeed: 30,
     loop:true
   });
+
+  function sendMail() {
+    const params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("messagebox").value
+    };
+    emailjs.send("service_1ruj0sv", "template_dg7hoqb", params)
+        .then(function(response) {
+            alert("Your email has been sent successfully!");
+            console.log("SUCCESS!", response.status, response.text);
+        })
+        .catch(function(error) {
+            alert("Failed to send the email. Please try again.");
+            console.log("FAILED...", error);
+        });
+}
